a29b5391c44ffb1f893343188b2498d97102d026
Andriy <senanven@gmail.com>
2021-05-27 22:14:18 +0300

    read more dtns

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,10 @@ document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
       parent.classList.toggle('accordion-item--activated')
     })
 )
+
+document.querySelectorAll('.read-more-btn').forEach((item) => 
+    item.addEventListener('click', () => {
+      const parent = item.parentNode;
+      parent.classList.toggle('read-more-active')
+    })
+)
